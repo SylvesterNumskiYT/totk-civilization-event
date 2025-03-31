0c0601fd0d46fb439c1792ff2273890544150b15
SylvesterNumskiYT <SylvesterNumski@gmail.com>
2025-03-31 18:30:21 -0400

    moved the git files to the right place
    I messed something up setting up the github so I'm fixing it so the files can be seen.

---
+++ src/main/kotlin/com/sylvesternumski/totkcivilizationevent.kt
@@ -0,0 +1,16 @@
+package com.sylvesternumski
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class totkcivilizationevent : JavaPlugin() {
+
+    companion object {
+        lateinit var instance: totkcivilizationevent
+    }
+
+    override fun onEnable() {
+    }
+
+    override fun onDisable() {
+    }
+}
